8d6f2b894b94d703e3801a512030661c2e8e8039
glgayathri <glgayathri@gmail.com>
2020-09-13 09:54:12 +0200

    diaplay required data on HTML DOM ++ changed design

--- oslo_sykkel/oslo_sykkel.js
+++ oslo_sykkel/oslo_sykkel.js
@@ -6,49 +6,36 @@ to create a small application that utilizes this api to show the stations and th
 You’re free to choose which language and libraries you use. How you show the stations and status is also up to you.
 */
 
-/* <div class="main-station-id">Majorstuen             *****
-                <div class="total">
-                    <p>Total</p>
-                    <div id="total-no">20</div>         *****
-                </div>
-                <div class="free">
-                    <p>Free</p>
-                    <div id="free-no">12</div>          ****
-                </div>
-                <div class="booked">
-                    <p>Booked</p>
-                    <div id="booked-no">8</div>         ****           */
-
 //declaring DOM elements
 const stationAddress = document.querySelector('.address');     //done
 const station = document.querySelector('.station');     //done
-
-
 const totalBikes = document.querySelector('.total-no');
 const freeBikes = document.querySelector('.free-no');
 const bookedBikes = document.querySelector('.booked-no');
  
-window.addEventListener('load', ()=>{
-    let long;
-    let lat;
-    var stations;
- 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-        /*Promise.allSettled(getStaionsAsync()).then(data => {
-            console.log(data);
-                stations=data;
-             });*/
-         getStationsAsync()
-         .then(data => {console.log("Stations", data);
-         getStationStatusAsync().then(stationInfo=>{console.log("Station info", stationInfo);
-         showStationInfo(combineResult(data, stationInfo));
-        });}); 
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  var stations;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      getStationsAsync()
+        .then(stationData => {
+          console.log("Stations", stationData);
+
+          getStationStatusAsync()
+            .then(stationInfo => {
+              console.log("Station info", stationInfo);
+              showStationInfo(combinedResult(stationData, stationInfo));
+            });
+        });
       //   showStationInfo(stations);   
-       })
-    }
+    })
+  }
 })
 
     async function getStationsAsync() 
@@ -65,61 +52,54 @@ async function getStationStatusAsync()
   return data;
 }
 
-function combineResult(stationInfo, stationStatus){
+function combinedResult(stationInfo, stationStatus){
     var combinedResult = [];
 
-    for (let i = 0;i< stationInfo.data.stations.length; i++){
-    var stationDetails = stationStatus.data.stations.find(station=> station.station_id=== stationInfo.data.stations[i].station_id);
-
-        // console.log("Station Detials ", stationDetails);
-        // console.log("Station name ", stationInfo.data.stations[i].name);
-        // console.log("Bikes Available ", stationDetails.num_bikes_available);
-
-        combinedResult.push({"stationId": stationDetails.station_id,
-        "stationName": stationInfo.data.stations[i].name,
-        "stationAddress": stationInfo.data.stations[i].address,
-        "totalBikes": stationInfo.data.stations[i].capacity,
-        "availableBikes": stationDetails.num_bikes_available,
-        "bookedBikes": stationDetails.num_docks_available});
+    for (let i = 0; i < stationInfo.data.stations.length; i++) {
+    var stationDetails = stationStatus.data.stations.find(station => station.station_id === stationInfo.data.stations[i].station_id);
+        
+    combinedResult.push({"stationId": stationDetails.station_id,
+                          "stationName": stationInfo.data.stations[i].name,
+                          "stationAddress": stationInfo.data.stations[i].address,
+                          "totalBikes": stationInfo.data.stations[i].capacity,
+                          "availableBikes": stationDetails.num_bikes_available,
+                          "bookedBikes": stationDetails.num_docks_available});
     }
     console.log(combinedResult);
     return combinedResult;
 }
 
-function showStationInfo (result) {
-    
+function showStationInfo(result) {
+
   for(let i=0; i < result.length; i++) {
     var stationName = document.createElement("div");
     stationName.id = "station" + i;
-    //stationName.className="address"
-    stationName.textContent= result[i].stationName;
+    stationName.textContent = `Station Name : ${result[i].stationName}`;    //Station Name
 
     var stationAddress = document.createElement("div");
     stationAddress.id = "address" + i;
     stationAddress.className="address"
-    stationAddress.textContent= result[i].stationAddress;
+    stationAddress.textContent = `Station Address : ${result[i].stationAddress}`;     //Station Address
     stationName.appendChild(stationAddress);
 
     var totalBikeAvailable = document.createElement("div");
     totalBikeAvailable.id = "Total" + i;
     totalBikeAvailable.className="total"
-    totalBikeAvailable.textContent= result[i].totalBikes;
+    totalBikeAvailable.textContent= `Total Bikes : ${result[i].totalBikes}`;      //Total Bikes
     stationName.appendChild(totalBikeAvailable);
 
     var freeBikes = document.createElement("div");
     freeBikes.id = "free" + i;
     freeBikes.className="free"
-    freeBikes.textContent= result[i].availableBikes;
+    freeBikes.textContent= `Free Bikes : ${result[i].availableBikes}`;       //Free Bikes
     stationName.appendChild(freeBikes);
 
     var reservedBikes = document.createElement("div");
     reservedBikes.id = "address" + i;
     reservedBikes.className="booked"
-    reservedBikes.textContent= result[i].bookedBikes;
-    stationName.appendChild(reservedBikes)
+    reservedBikes.textContent = `Booked Bikes : ${result[i].bookedBikes}`;       //Booked Bikes
+    stationName.appendChild(reservedBikes);
 
     station.appendChild(stationName);
  }   
 }
-
-
